1eeb68be6e7ca5ab2999aaedb3595573fa5b95c1
Manika Tyagi <94793244+manikatyagi@users.noreply.github.com>
2023-03-30 16:09:02 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (79.73%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -1,43 +1,44 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        stack<char>s1;
-        stack<char>s2;
         
-        
-        for(int i=0;i<s.size();i++){
-            if(!s1.empty() and s[i]=='#'){
-                s1.pop();
+        int i=s.size()-1;
+        int j=t.size()-1;
+        int back;
+        while(true){
+            
+             back=0;
+            
+            while(i>=0 and (back>0 or s[i]=='#')){
+                if(s[i]=='#'){
+                    back++;
+                }
+                else{
+                    back--;
+                }
+                i--;
             }
-            else if(s[i]!='#'){
-                s1.push(s[i]);         // f
+            
+            back=0;
+            
+            while(j>=0 and (back>0 or t[j]=='#')){
+                if(t[j]=='#'){
+                    back++;
+                }
+                else{
+                    back--;
+                }
+                j--;
             }
+            
+            if(i>=0 and j>=0 and s[i]==t[j]){
+                i--;
+                j--;
+            }
+            else
+                break;
         }
-        
-        for(int i=0;i<t.size();i++){
-            if(t[i]=='#' and !s2.empty()){
-                s2.pop();
-            }
-            else if(t[i]!='#'){
-                s2.push(t[i]);      // f
-            }
-        }
-        cout<<s1.size()<<" ";
-        cout<<s2.size()<<" ";
-        if(s1.size()!=s2.size())return false;
-        
-       while(!s1.empty() and !s2.empty()){
-           cout<<s1.top()<<" ";
-           cout<<s2.top()<<" ";
-          if(s1.top()!=s2.top())return false;
-           s1.pop();
-           s2.pop();
-       }
-  
       
-        
-        return true;
-       
-        
+        return i==-1 and j==-1;
     }
 };
