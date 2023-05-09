fa994d86d73e65eb5ae4b20c3c5d519c3a3adcd5
Manika Tyagi <94793244+manikatyagi@users.noreply.github.com>
2023-05-09 20:38:54 +0530

    Time: 186 ms (11.57%), Space: 56.4 MB (18.21%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,40 +1,36 @@
 class Solution {
 public:
-    void dfs(vector<int> adj[], vector<bool>&vis ,int node){
-        vis[node]=1;
-        
-        for(auto x:adj[node]){
-            if(!vis[x]){
-                dfs(adj,vis,x);
+    
+    void dfs(vector<int>&vis,vector<vector<int>>&adj, int n,int curr){
+        vis[curr]=1;
+        for(auto x:adj[curr]){
+            if(vis[x]==0){
+               
+                dfs(vis,adj,n,x);
             }
         }
     }
     int makeConnected(int n, vector<vector<int>>& conn) {
-        // adj matrix 
-        //    0        1     2     3    4   5 
-        // [[1,2 3] [0 2 3] [0 1] [0 1] [] []]
-        vector<int>adj[n];
         
-        for(auto v:conn){
-            adj[v[0]].push_back(v[1]);
-            adj[v[1]].push_back(v[0]);  
+        if(conn.size()<n-1)return -1;
+        
+        vector<vector<int>>adj(n);
+        
+        for(auto i:conn){
+            adj[i[0]].push_back(i[1]);
+            adj[i[1]].push_back(i[0]);
         }
-        //  0 1 2 3 4 5
-        // [0 0 0 0 0 0]
-        vector<bool>vis(n,0);
         
+        vector<int>vis(n,0);
+        
+       // dfs(vis,adj,n,0);
         int c=0;
-        int m=conn.size();
-        
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                c++;    // 1
-                dfs(adj,vis,i);
+            if(vis[i]==0){
+                c++;
+                dfs(vis,adj,n,i);
             }
         }
-        
-        if(m>=n-1)return c-1;
-        
-        return -1;
+        return c-1;
     }
 };
