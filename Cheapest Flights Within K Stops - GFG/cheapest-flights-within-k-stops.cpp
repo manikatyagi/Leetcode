9c0f1ed6044bc749c957bb7c84572731e365eec2
Manika Tyagi <94793244+manikatyagi@users.noreply.github.com>
2023-05-10 12:29:04 +0530

    Added solution - LeetHub

---
+++ Cheapest Flights Within K Stops - GFG/cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int CheapestFLight(int n, vector<vector<int>>& flights, int src, int dst, int K)  {
+        // Code here
+        
+        vector<vector<int>>adj[n];
+        
+        for(auto it:flights){
+            adj[it[0]].push_back({it[1],it[2]});
+        }
+        
+        queue<pair<int,pair<int,int>>>q;
+        
+        vector<int>dist(n,INT_MAX);
+        
+        q.push({0,{src,0}});
+        
+        dist[src]=0;
+        
+        while(!q.empty()){
+            auto it=q.front();
+            
+            int node=it.second.first;
+            int cost=it.second.second;
+            int stops=it.first;
+            
+            if(stops>K)break;
+            
+            q.pop();
+            
+            for(auto x:adj[node]){
+                int edgeW=x[1];
+                int adjNode=x[0];
+                
+                if(dist[adjNode]>edgeW+cost and stops<=K){
+                    dist[adjNode]=edgeW+cost;
+                    
+                    q.push({stops+1,{adjNode,dist[adjNode]}});
+                
+                }
+            }
+        }
+        if(dist[dst]==INT_MAX)return -1;
+        
+        return dist[dst];
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n; cin>>n;
+        int edge; cin>>edge;
+        vector<vector<int>> flights;
+        
+        for(int i=0; i<edge; ++i){
+            vector<int> temp;
+            for(int j=0; j<3; ++j){
+                int x; cin>>x;
+                temp.push_back(x);
+            }
+            flights.push_back(temp);
+        }
+        
+        int src,dst,k;
+        cin>>src>>dst>>k;
+        Solution obj;
+        cout<<obj.CheapestFLight(n,flights,src,dst,k)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
