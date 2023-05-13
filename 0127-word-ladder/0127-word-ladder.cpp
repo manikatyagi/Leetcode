4885968503df1f0a07112bbaa58977e2583bf8f6
Manika Tyagi <94793244+manikatyagi@users.noreply.github.com>
2023-05-13 13:01:12 +0530

    Time: 1498 ms (12.79%), Space: 27.4 MB (18.96%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -10,34 +10,30 @@ public:
         int ans=1;
         
         while(!q.empty()){
-           
+            
             int n=q.size();
             
             while(n-->0){
-                string f=q.front();  // hit
-                q.pop();
                 
+                string f=q.front();
+                q.pop();
                 if(f==endWord)return ans;
                 
                 se.erase(f);
-                
                 for(int i=0;i<f.size();i++){
-                    
-                    char c=f[i];   // h i
+                    char c=f[i];
                     
                     for(int j=0;j<26;j++){
-                        f[i]='a'+j;   // 
+                        f[i]='a'+j;
                         
                         if(se.find(f)!=se.end()){
                             q.push(f);
                         }
-                        
                     }
                     f[i]=c;
                 }
             }
-            ans++; 
-            
+            ans++;
         }
        return 0; 
     }
