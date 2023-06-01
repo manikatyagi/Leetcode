a0be086236a99b2430ba1806bcffbbddf04cfa66
Manika Tyagi <94793244+manikatyagi@users.noreply.github.com>
2023-06-01 22:29:39 +0530

    Time: 298 ms (12.79%), Space: 33.3 MB (18.74%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -6,36 +6,32 @@ public:
         
         if(grid[0][0]==1 or grid[n-1][n-1]==1)return -1;
         
-         priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
-    
+        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
+        
         pq.push({0,0,0});
         vector<vector<int>>vis(n,vector<int>(n,0));
         int ans=INT_MAX;
         int dir[8][2]={{0,1},{1,0},{0,-1},{-1,0},{-1,-1},{1,1},{-1,1},{1,-1}};
         while(!pq.empty()){
             auto t=pq.top();
-            
-            int d=t[0];
-            int i=t[1];
-            int j=t[2];
-            
             pq.pop();
+            int d=t[0];
+            int x=t[1];
+            int y=t[2];
             
-            if(i==n-1 and j==n-1 )ans=min(ans,d);
-            for(int k=0;k<8;k++){
-                int r=i+dir[k][0];
-                int c=j+dir[k][1];
+            if(x==n-1 and y==n-1)ans=min(ans,d);
+            
+            for(int i=0;i<8;i++){
+                int r=x+dir[i][0];
+                int c=y+dir[i][1];
                 
-                if(r>=0 and r<grid.size() and c>=0 and c<grid.size() and grid[r][c]==0 and vis[r][c]==0){
-                    int t=d+1;
-                    
-                    pq.push({t,r,c});
+                if(r>=0 and c>=0 and r<n and c<n and grid[r][c]==0 and vis[r][c]==0){
                     vis[r][c]=1;
+                    int dis=d+1;
+                    pq.push({dis,r,c});
                 }
-                
             }
-            
-            
+                
         }
         return ans==INT_MAX?-1:ans+1;
     }
