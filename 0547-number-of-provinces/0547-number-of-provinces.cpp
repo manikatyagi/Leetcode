1ef49a0d351b44f74cdd171f39f28b017c4fcf3b
Manika Tyagi <94793244+manikatyagi@users.noreply.github.com>
2023-06-04 17:19:47 +0530

    Time: 15 ms (99.46%), Space: 14.6 MB (43.90%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -12,22 +12,23 @@ public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size(), m = isConnected[0].size();
         vector<vector<int>>graph(n);
-        // [[0 1] [0 1] []]
-        vector<int>vis(n,0);
+        // [[0 1] [0 1] [2]]
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
+                
                 if(isConnected[i][j]==1){
                     graph[i].push_back(j);
                 }
             }
         }
+        vector<int>vis(n,0);
         int ans=0;
         for(int i=0;i<n;i++){
             if(vis[i]==0){
-                dfs(i,vis,graph);
                 ans++;
-                
+                dfs(i,vis,graph);
             }
+                
         }
         return ans;
     }
